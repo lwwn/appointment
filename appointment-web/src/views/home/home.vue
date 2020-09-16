f7437358780aa021010d37aed9a818450f41edaa
lwwn <365177686@qq.com>
2020-09-16 18:00:57 +0800

    deng

--- appointment-web/src/views/home/home.vue
+++ appointment-web/src/views/home/home.vue
@@ -1,5 +1,23 @@
 <template>
-    <div> 首页</div>
+    <div> 
+        <header>
+            <h3>神通有限公司</h3>
+            <p>山有扶苏，隰有荷华。不见子都，乃见狂且。山有桥松，隰有游龙。不见子充，乃见狡童。</p>
+        </header>
+        <nav>
+            <ul>
+                <li><span>首页</span></li>
+                <li><span>日志</span></li>
+            </ul>
+            <ul>
+                <li><span>登录</span></li>
+                <li>
+                    <div><img src="" alt=""></div>
+                    <div><span>退出</span></div>
+                </li>
+            </ul>
+        </nav>
+    </div>
 </template>
 <script>
 export default {
--- appointment-web/src/views/userCenter/user.vue
+++ appointment-web/src/views/userCenter/user.vue
@@ -1,5 +1,7 @@
 <template>
-     <div>我的</div>
+     <div>
+          
+     </div>
 </template>
 <script>
 export default {
